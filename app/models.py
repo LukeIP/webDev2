f520d796f19c23d89e33f8faa118205ab12b2055
LukeIP <124840529+LukeIP@users.noreply.github.com>
2023-11-28 23:49:53 +0000

    Generated Model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 
 # linking tables
-groups = db.Table('group',
+groups = db.Table('groups',
                   db.Column('user_id', db.Integer,
                             db.ForeignKey('user.id'), primary_key=True),
                   db.Column('group_id', db.Integer,
@@ -22,15 +22,15 @@ class User(db.Model):
 
 
 class Post(db.Model):
-    id = db.column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(500), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    group_id = db.Column(db.Integer, db.ForeignKey('group.id', nullable=True))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=True)
     likes = db.relationship('user', secondary=likes, backref='liked_posts')
 
 
 class Group(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    name = db.column(db.String(500), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(500), nullable=False)
     posts = db.relationship('group', backref='group')
     users = db.relationship('user', secondary=groups, backref='groups')
--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from flask import redirect, render_template
 from app import app, models
 
+@app.route('/users/<id>.html')
+def show_wall(id):
+    user = models.User.one_or_404(id=id)
+    return render_template('user_wall.html', user=user)
